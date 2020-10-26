b51278a34d4456114df669a2a62db0e9f9780582
Nilou Mostofi <nilou@km-direct.com>
2020-10-26 09:32:20 -0700

    update api-routes with update route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,33 +1,50 @@
-const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
 
-router.post("/api/workout", ({ body }, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+router.post('/api/workout', (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/workout/range", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+router.get('/api/workout/range', ({body}, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.put('/api/workouts/:id', ({body, params}, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    {$push: {exercises: body}},
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-router.get("/api/workout", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+router.get('/api/workout', (req, res) => {
+  Workout.find({})
+    .sort({date: -1})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
