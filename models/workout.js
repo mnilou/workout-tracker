728d616bf26efeb14c4872d21df92388fe84aaab
Nilou Mostofi <nilou@km-direct.com>
2020-10-26 09:12:01 -0700

    models and routes

---
+++ models/workout.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Please enter an exercise type.',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Please enter an exercise name.',
+      },
+      duration: {
+        type: Number,
+        required: 'Please enter the duration.',
+      },
+      weight: {
+        type: Number       
+      },
+      reps: {
+        type: Number       
+      },
+      sets: {
+        type: Number       
+      },
+      distance: {
+        type: Number       
+      },
+    },
+  ],
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+}
+);
+workoutSchema.virtual("total duration").get(function(){
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0)
+})
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
---
+++ routes/html-routes.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const router = require("express").Router();
+
+// "/notes" responds with the notes.html file
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+// All other routes respond with the index.html file
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+// All other routes respond with the index.html file
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
